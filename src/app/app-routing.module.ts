c6ad033b2cbfe9fde57c9261c39c9639885197eb
Bierman <niels.bierman@hva.nl>
2019-10-08 16:34:44 +0200

    Implemented routing, profile page, navigation to profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
+import {ProfilepageComponent} from './components/profilepage/profilepage.component';
+import {MainpageComponent} from './components/mainpage/mainpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainpageComponent},
+  {path: 'profile', component: ProfilepageComponent}
+];
 
 @NgModule({
+  declarations: [
+  ],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MainpageComponent } from './components/mainpage/mainpage.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {MainpageComponent} from './components/mainpage/mainpage.component';
+import {ProfilepageComponent} from './components/profilepage/profilepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainpageComponent
+    MainpageComponent,
+    ProfilepageComponent
   ],
   imports: [
     BrowserModule,
@@ -17,4 +19,5 @@ import { MainpageComponent } from './components/mainpage/mainpage.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
