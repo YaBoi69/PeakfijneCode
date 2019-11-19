d3b3508be5c96077b4fadacbe053899c136b3ce2
Bierman <niels.bierman@hva.nl>
2019-11-19 12:04:51 +0100

    Big fix

--- src/app/services/points.service.ts
+++ src/app/services/points.service.ts
@@ -7,21 +7,18 @@ import {SessionService} from './session.service';
   providedIn: 'root'
 })
 export class PointsService {
-  points = new EventEmitter<number>();
-  currentUser: User;
   constructor(private sessionService: SessionService) {
-    this.currentUser = new User('jandejaap@gmail.com', 'wachtwoord123', 'Jantje', false);
   }
 
   postAddView(post: Post) {
-    // const currentUser = this.sessionService.getUserRepository().getAll()[0]; // get faked current user
+    const currentUser = this.sessionService.getUserRepository().getAll()[0]; // get faked current user
      // get fake faked current user
     const oldViews = post.getViews(); // get the views of the post
     const pointArray = [10, 8, 6, 4, 2, 1]; // get faked amount of points from settings
-    if (!oldViews.includes(this.currentUser)) { // check if the player is in the point bracket
+    if (!oldViews.includes(currentUser)) { // check if the player is in the point bracket
       if (oldViews.length <= pointArray.length) {
-        this.currentUser.givePoints(pointArray[oldViews.length]); // add points to the player
-        post.addView(this.currentUser); // add the viewer to the post
+        currentUser.givePoints(pointArray[oldViews.length]); // add points to the player
+        post.addView(currentUser); // add the viewer to the post
       } else {
         // TODO show message: sorry there couldn't be any points obtained
         console.log("no points left");
@@ -30,6 +27,6 @@ export class PointsService {
       // TODO show message: you have already opened this message
       console.log("message already opened");
     }
-    console.log(this.currentUser.getPoints());
+    console.log(currentUser.getPoints());
   }
 }
